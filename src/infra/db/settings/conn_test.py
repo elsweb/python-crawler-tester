8255d362bd4e1f60e067a4710487c65d4839ac7a
Emanuel Lds <toolsjob.neodeon@gmail.com>
2024-05-12 16:39:47 -0300

    feat: pytest

--- src/infra/db/settings/conn_test.py
+++ src/infra/db/settings/conn_test.py
@@ -1,5 +1,7 @@
+import pytest
 from .conn import DBConnectionHandler
 
+@pytest.mark.skip(reason="Sensive test")
 def test_create_database_engine():
     db_conn_ = DBConnectionHandler()
     engine = db_conn_.get_engine()
