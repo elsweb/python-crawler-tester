180c4b67eac05f25c6523cea0966cc691d8fffc7
Emanuel Lds <toolsjob.neodeon@gmail.com>
2024-05-12 16:12:55 -0300

    feat: created conn

---
+++ src/infra/db/settings/conn.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+class DBConnectionHandler:
+
+    def __init__(self) -> None:
+        self.__connection_string = "{}://{}:{}@{}:{}/{}".format(
+            'mysql+pymysql',
+            os.getenv('DB_USER'),
+            os.getenv('DB_PASSWORD'),
+            os.getenv('DB_HOST'),
+            os.getenv('DB_PORT'),
+            os.getenv('DB_DATABASE')
+        )
+        self.__engine = self.__create_database_engine()
+        self.session = None
+
+    def __create_database_engine(self):
+        engine = create_engine(self.__connection_string)
+        return engine
+
+    def get_engine(self):
+        return self.__engine
+
+    def __enter__(self):
+        session_make = sessionmaker(bind=self.__engine)
+        self.session = session_make()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
